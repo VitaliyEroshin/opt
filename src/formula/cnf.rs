2db6c9570880088082d106692b3282fe79db4401
vitaliy <vitaliy-eroshin@yandex.ru>
2022-08-04 01:26:54 +0300

    Now DP is working, added simple test

--- src/formula/cnf.rs
+++ src/formula/cnf.rs
@@ -1,28 +1,46 @@
-use std::collections::HashSet;
+use std::{collections::HashSet, mem::swap, hash::Hash};
 
-#[derive(Eq, Hash, PartialEq, Clone, Copy, Debug)]
+#[derive(Eq, Hash, PartialEq, Clone, Copy, Debug, PartialOrd, Ord)]
 pub struct Literal {
     pub var: usize,
     pub sign: bool,
 }
 
+impl Literal {
+    pub fn neg(&self) -> Literal {
+        return Literal {
+            var: self.var,
+            sign: !self.sign,
+        };
+    }
+}
+
 pub struct CNF {
-    clauses: Vec<Vec<Literal>>,
+    clauses: HashSet<Vec<Literal>>,
 }
 
 impl CNF {
     pub fn new() -> CNF {
         CNF {
-            clauses: Vec::new(),
+            clauses: HashSet::new(),
         }
     }
 
     pub fn add_clause(&mut self, clause: Vec<Literal>) {
-        self.clauses.push(clause);
+        self.clauses.insert(clause);
     }
 
-    pub fn get_clauses(&mut self) -> &mut Vec<Vec<Literal>> {
+    pub fn get_clauses(&mut self) -> &mut HashSet<Vec<Literal>> {
         return &mut self.clauses;
+    }
+
+    pub fn has_empty_clause(&self) -> bool {
+        for clause in self.clauses.iter() {
+            if clause.is_empty() {
+                return true;
+            }
+        }
+        return false;
     }
 }
 
@@ -33,16 +51,9 @@ impl SATSolver {
         return clause.len() == 1;
     }
 
-    fn get_negative_literal(literal: &Literal) -> Literal {
-        return Literal {
-            var: literal.var,
-            sign: !literal.sign,
-        };
-    }
-
     pub fn unit_propagation(mut cnf: CNF) -> CNF {
         let mut unit_clauses = HashSet::<Literal>::new();
-        let clauses: &mut Vec<Vec<Literal>> = cnf.get_clauses();
+        let clauses: &mut HashSet<Vec<Literal>> = cnf.get_clauses();
 
         for clause in clauses.iter() {
             if Self::is_unit_clause(&clause) {
@@ -50,99 +61,178 @@ impl SATSolver {
             }
         }
 
-        let mut index: usize = 0;
-        while index != clauses.len() {
-            let clause: &mut Vec<Literal> = &mut clauses[index];
+        let mut removed_clauses = HashSet::<Vec<Literal>>::new();
+        let mut add_clauses = HashSet::<Vec<Literal>>::new();
+        for mut clause in clauses.iter() {
             if Self::is_unit_clause(&clause) {
-                clauses.swap_remove(index);
+                removed_clauses.insert(clause.clone());
                 continue;
             }
-            
-            let mut literal_index: usize = 0;
-            while literal_index != clause.len() {
-                let literal = &clause[literal_index];
-                if unit_clauses.contains(&Self::get_negative_literal(&literal)) {
-                    clause.swap_remove(literal_index);
-                    continue;
+
+            for index in 0..clause.len() {
+                if unit_clauses.contains(&clause[index].neg()) {
+                    let mut new_clause = clause.clone();
+                    new_clause.swap_remove(index);
+
+                    removed_clauses.insert(clause.clone());
+                    add_clauses.insert(new_clause);
+                    break;
                 }
-                literal_index += 1;
             }
-            index += 1;
+        }
+
+        for clause in removed_clauses.iter() {
+            clauses.remove(clause);
+        }
+
+        for clause in add_clauses.into_iter() {
+            clauses.insert(clause);
         }
         
         return cnf;
     }
 
     pub fn normalize_cnf(mut cnf: CNF) -> CNF {
-        let clauses: &mut Vec<Vec<Literal>> = cnf.get_clauses();
-        let mut index: usize = 0;
-        while index != clauses.len() {
-            let clause: &mut Vec<Literal> = &mut clauses[index];
+        let clauses: &mut HashSet<Vec<Literal>> = cnf.get_clauses();
+        
+        let mut removed_clauses = HashSet::<Vec<Literal>>::new();
+        for clause in clauses.iter() {
             let mut known_literals = HashSet::<Literal>::new();
             let mut true_clause = false;
 
-            for literal in clause {
-                if known_literals.contains(&Self::get_negative_literal(&literal)) {
+            for literal in clause.iter() {
+                if known_literals.contains(&literal.neg()) {
+                    removed_clauses.insert(clause.clone());
                     true_clause = true;
                     break;
                 }
                 known_literals.insert(literal.clone());
             }
+        }
 
-            if true_clause {
-                clauses.swap_remove(index);
-                continue;
-            } 
-            index += 1;
+        for clause in removed_clauses.iter() {
+            clauses.remove(clause);
         }
 
         cnf
     }
 
     pub fn pure_literal_ellimination(mut cnf: CNF) -> CNF {
-        let clauses: &mut Vec<Vec<Literal>> = cnf.get_clauses();
+        let clauses: &mut HashSet<Vec<Literal>> = cnf.get_clauses();
         let mut known_literals = HashSet::<Literal>::new();
         let mut ellimination_whitelist = HashSet::<Literal>::new();
 
         for clause in clauses.iter() {
             for literal in clause {
-                if known_literals.contains(&Self::get_negative_literal(&literal)) {
+                if known_literals.contains(&literal.neg()) {
                     ellimination_whitelist.insert(literal.clone());
-                    ellimination_whitelist.insert(Self::get_negative_literal(literal));
+                    ellimination_whitelist.insert(literal.neg());
                 } else {
                     known_literals.insert(literal.clone());
                 }
             }
         }
 
-        let mut index: usize = 0;
-        while index != clauses.len() {
-            let clause: &mut Vec<Literal> = &mut clauses[index];
-            let mut elliminate = false;
-
+        let mut removed_clauses = HashSet::<Vec<Literal>>::new();
+        for clause in clauses.iter() {
             for literal in clause {
                 if !ellimination_whitelist.contains(&literal) {
-                    elliminate = true;
+                    removed_clauses.insert(clause.clone());
                     break;
                 }
             }
+        }
 
-            if elliminate {
-                clauses.swap_remove(index);
-                continue;
-            }
-
-            index += 1;
+        for clause in removed_clauses.iter() {
+            clauses.remove(clause);
         }
 
         cnf
     }
 
-    fn solve(mut cnf: CNF) -> (bool, CNF) {
-        cnf = Self::unit_propagation(cnf);
-        cnf = Self::normalize_cnf(cnf);
+    fn remove_literal(mut clause: Vec<Literal>, literal: &Literal) -> Vec<Literal> {
+        let mut index: usize = 0;
+        while index != clause.len() {
+            if clause[index] == *literal {
+                clause.swap_remove(index);
+                return clause;
+            }
+            index += 1;
+        }
+        return clause;
+    }
 
-        (true, cnf)
+    pub fn resolute(mut a: Vec<Literal>, mut b: Vec<Literal>, l: Literal) -> Option<Vec<Literal>> {
+        if a.contains(&l.neg()) && b.contains(&l) {
+            swap(&mut a, &mut b);
+        } else if !a.contains(&l) || !b.contains(&l.neg()) {
+            return None;
+        }
+        a = Self::remove_literal(a, &l);
+        b = Self::remove_literal(b, &l.neg());
+        let mut result = [a, b].concat();
+        result.sort();
+        result.dedup();
+        Some(result)
+    }
+
+    pub fn davis_putnam_procedure(mut cnf: CNF) -> CNF {
+        let clauses: &mut HashSet<Vec<Literal>> = cnf.get_clauses();
+        let mut all_literals = HashSet::<Literal>::new();
+
+        for clause in clauses.iter() {
+            for literal in clause {
+                all_literals.insert(literal.clone());
+            }
+        }
+
+        for l in all_literals {
+            let mut new_clauses: HashSet<Vec<Literal>> = HashSet::new();
+            let mut removed_clauses: HashSet<Vec<Literal>> = HashSet::new();
+
+            for a in clauses.iter() {
+                for b in clauses.iter() {
+                    let resolvent = Self::resolute(a.clone(), b.clone(), l);
+                    
+                    if resolvent.is_some() {
+                        let resolvent = &resolvent.unwrap();
+                        new_clauses.insert(resolvent.to_vec());
+                    }
+                }
+            }
+
+            for clause in clauses.iter() {
+                if clause.contains(&l) || clause.contains(&l.neg()) {
+                    removed_clauses.insert(clause.clone());
+                }
+            }
+
+            for clause in removed_clauses.iter() {
+                clauses.remove(clause);
+            }
+            for clause in new_clauses.iter() {
+                clauses.insert(clause.clone());
+            }
+        }
+
+        cnf
+    }
+
+    pub fn solve(mut cnf: CNF) -> (bool, CNF) {
+        loop {
+            println!("New iteration, cnf size is {}", cnf.get_clauses().len());
+            cnf = Self::unit_propagation(cnf);
+            cnf = Self::normalize_cnf(cnf);
+            cnf = Self::pure_literal_ellimination(cnf);
+            if cnf.get_clauses().is_empty() {
+                return (true, cnf);
+            }
+
+            if cnf.has_empty_clause() {
+                return (false, cnf);
+            }
+            cnf = Self::davis_putnam_procedure(cnf);
+        }
     }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 mod formula;
 use crate::formula::cnf::{CNF, SATSolver, Literal};
+use std::collections::HashSet;
 
 fn print_cnf(cnf: &mut CNF) {
     let clauses = cnf.get_clauses();
-    for clause in clauses {
+    for clause in clauses.iter() {
         println!("{:?}", clause);
     }
 }
@@ -17,7 +18,8 @@ fn unit_propagation_test() {
     
     cnf = SATSolver::unit_propagation(cnf);
 
-    assert_eq!(cnf.get_clauses(), &vec![vec![Literal { var: 2, sign: true}]]);
+    let result: Vec<_> = cnf.get_clauses().clone().into_iter().collect();
+    assert_eq!(result, vec![vec![Literal { var: 2, sign: true}]]);
     println!("Unit propagation test passed!");
 }
 
@@ -29,7 +31,8 @@ fn normalization_test() {
     
     cnf = SATSolver::normalize_cnf(cnf);
 
-    assert_eq!(cnf.get_clauses(), &vec![
+    let result: Vec<_> = cnf.get_clauses().clone().into_iter().collect();
+    assert_eq!(result, vec![
         vec![Literal { var: 0, sign: true }],
         vec![Literal { var: 2, sign: true }, Literal { var: 0, sign: false }]
     ]);
@@ -41,10 +44,44 @@ fn pure_literal_ellimination_test() {
     cnf.add_clause(vec![Literal {var: 0, sign: false}, Literal {var: 1, sign: true}]);
 
     cnf = SATSolver::pure_literal_ellimination(cnf);
-    
-    assert_eq!(cnf.get_clauses(), &vec![vec![Literal {var: 0, sign: true}]]);
+
+    let result: Vec<_> = cnf.get_clauses().clone().into_iter().collect();
+    assert_eq!(result, vec![vec![Literal {var: 0, sign: true}]]);
+}
+
+
+use std::io::{self, BufRead};
+use std::fs::File;
+
+fn get_cnf_from_file() -> CNF {
+    let file = File::open("./test.txt").unwrap();
+    let reader = io::BufReader::new(file);
+
+    let mut cnf = CNF::new();
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let mut clause = Vec::new();
+        for literal in line.split(" ") {
+            let mut literal = literal.parse::<i32>().unwrap();
+            let sign = literal < 0;
+            if sign {
+                literal = literal.abs();
+            }
+
+            clause.push(Literal {var: literal as usize, sign: sign});
+        }
+        clause.pop();
+        cnf.add_clause(clause);
+    }
+    cnf
 }
 
 fn main() {
+    unit_propagation_test();
+    normalization_test();
+    pure_literal_ellimination_test();
 
+    let mut cnf = get_cnf_from_file();
+    let (res, cnf) = SATSolver::solve(cnf);
+    println!("{:?}", res);
 }
